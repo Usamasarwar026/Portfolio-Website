ec97c04288e788db314b7e2dbd69c1da29933b33
usamasarwar026 <usamasarwar160492@gmail.com>
2026-07-11 15:07:40 +0500

    replace index.js with the api/contact.js

--- server/api/contact.js
+++ server/api/contact.js
@@ -44,5 +44,4 @@ app.post('/api/contact', async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+export default app;
